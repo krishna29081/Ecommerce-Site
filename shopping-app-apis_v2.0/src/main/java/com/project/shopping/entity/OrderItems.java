3d0d162fab8e8cb7654630d4cebcdc1fc7efa40d
Krishna <krishna.aggarwal123456789@gmail.com>
2023-02-26 12:20:29 +0530

    Place order API

--- shopping-app-apis_v2.0/src/main/java/com/project/shopping/entity/OrderItems.java
+++ shopping-app-apis_v2.0/src/main/java/com/project/shopping/entity/OrderItems.java
@@ -1,9 +1,11 @@
 package com.project.shopping.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
@@ -15,17 +17,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class OrderItem {
-	
+public class OrderItems {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private int orderItemId;
 
-@OneToOne
+@OneToOne(cascade = CascadeType.MERGE)
+@JoinColumn(name = "productId")
 private Products product;
 
-@ManyToOne
+private double productCartQuantity;
+private double totalProductPrice;
+
+@ManyToOne(cascade = CascadeType.PERSIST)
 private Orders order;
 
-
+	
 }
